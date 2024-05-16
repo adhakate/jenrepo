f6ce8945dc1278f7a1523016c78c71ece5d20c9d
devseri <ankitadhakate29@gmail.com>
2024-05-16 08:53:54 +0530

    add abcdef

--- continuous_integration/app.js
+++ continuous_integration/app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: 'Changed message add changes.' });
+  res.status(200).json({ message: 'Changed message.' });
 });
 
 module.exports = app;
